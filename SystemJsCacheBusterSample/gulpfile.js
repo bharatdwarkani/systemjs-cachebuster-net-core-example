906f35f56c041329bd6c01b33762778df15b4ad4
Bharat Dwarkani <bharatdwarkanim@syncfusion.com>
2018-04-18 11:37:50 +0530

    Included functionality for versioning typescript files using systemjs-cachebuster

---
+++ SystemJsCacheBusterSample/gulpfile.js
@@ -0,0 +1,30 @@
+﻿/// <binding BeforeBuild='cache-burst-ts, copy-system-cache-buster' />
+"use strict";
+var gulp = require("gulp"),    
+    replace = require('gulp-replace'),
+    SystemJSCacheBuster = require("systemjs-cachebuster"),
+    cacheBuster = new SystemJSCacheBuster(); 
+
+var tsCacheBurstercopied = "./wwwroot/js/Others/system.cachebuster.json";
+var tsCacheBurster = "./system.cachebuster.json";
+
+
+/* Cache bursting for ts files */
+
+var tsfiles = "./wwwroot/ts/*.ts";
+gulp.task('cache-burst-ts', function () {
+    return gulp
+        .src(tsfiles)
+        .pipe(cacheBuster.full())
+        .pipe(cacheBuster.incremental());
+});
+
+/* copy cache burster file to wwwroot */
+
+gulp.task('copy-system-cache-buster', function () {
+    return gulp.src(['system.cachebuster.json'])
+        .pipe(replace('wwwroot/', ''))
+        .pipe(gulp.dest('./wwwroot/js/others/'));
+});
+
+
